567d9b7cb83aa7b1ed4d25141831b2f6340d22b5
IagoGonçalves22 <iago.g.goncalves@gmail.com>
2023-03-05 14:39:37 -0300

    configuring firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyCR-AKVVz08OHtXN6mebhmFoMAORKkv4bI',
+    authDomain: 'easy-store-b0375.firebaseapp.com',
+    projectId: 'easy-store-b0375',
+    storageBucket: 'easy-store-b0375.appspot.com',
+    messagingSenderId: '790891904561',
+    appId: '1:790891904561:web:43c54de6ba984ad2415f10'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
