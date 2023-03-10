6661da9c13a7e21de8a07907d5dc5b082a3c0bb4
IagoGonçalves22 <iago.g.goncalves@gmail.com>
2023-03-09 22:53:55 -0300

    create LoginPage.scss

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 // import { DataTable, Page, Text } from '@7shifts/sous-chef';
-
+import './LoginPage.scss';
 const LoginPage = () => {
-    return <h1>Login Page</h1>;
+    return <h1 className="login-page">Login Page</h1>;
 };
 export default LoginPage;
